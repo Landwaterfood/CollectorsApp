8fe68b487bc49b4c40985c818149f628159524c7
chris <landwaterfood@gmail.com>
2024-09-23 14:51:25 +0100

    add files for review

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="php" href="php.php">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php require_once 'php.php';?>
 </head>
 <body>
+
+foreach
 
 
 
